92aaf721eb0aeb7884ba366ead114c08a6e157a1
ROHIT12-bit <reddyrohit823@gmail.com>
2025-11-14 21:23:15 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+class Config:
+    API_ID = 12345
+    API_HASH = "your_api_hash"
+    BOT_TOKEN = "your_bot_token"
+
+    START_MSG = """
+👋 **Welcome to Anime Universe Banner Bot!**
+
+🎨 I can generate awesome Anime Banners with:
+• Anime image
+• Title & rating
+• Genres
+• Description
+• “Anime Universe” branding
+
+📌 Use:
+/banner <anime name>
+
+Example:
+/banner naruto
+/banner demon slayer
+"""
